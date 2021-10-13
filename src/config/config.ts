71fa27d263844ffd8923d06c094191a0cdb6ff3a
script-money <74860630+script-money@users.noreply.github.com>
2021-10-13 20:11:31 +0800

    🚧 commit code for review

--- src/config/config.ts
+++ src/config/config.ts
@@ -4,18 +4,12 @@ config({
   env: 'local',
   'accessNode.api': import.meta.env.VITE_FLOW_HTTP_URL,
   'discovery.wallet': import.meta.env.VITE_WALLET_DISCOVERY,
-  // '0xFungibleToken': import.meta.env.VITE_FLOW_FUNGIBLETOKEN,
-  // '0xNonFungibleToken': import.meta.env.VITE_FLOW_NONFUNGIBLETOKEN,
-  // '0xCCSToken': import.meta.env.VITE_PROJECT_CCSTOKEN,
-  // '0xBallotContract': import.meta.env.VITE_PROJECT_BALLOTCONTRACT,
-  // '0xActivityContract': import.meta.env.VITE_PROJECT_ACTIVITYCONTRACT,
-  // '0xMemorials': import.meta.env.VITE_PROJECT_MEMORIALS
-  '0xFungibleToken': '0xee82856bf20e2aa6',
-  '0xNonFungibleToken': '0xf8d6e0586b0a20c7',
-  '0xCCSToken': '0xf8d6e0586b0a20c7',
-  '0xBallotContract': '0xf8d6e0586b0a20c7',
-  '0xActivityContract': '0xf8d6e0586b0a20c7',
-  '0xMemorials': '0xf8d6e0586b0a20c7'
+  '0xFungibleToken': import.meta.env.VITE_FLOW_FUNGIBLETOKEN,
+  '0xNonFungibleToken': import.meta.env.VITE_FLOW_NONFUNGIBLETOKEN,
+  '0xCCSToken': import.meta.env.VITE_PROJECT_CCSTOKEN,
+  '0xBallotContract': import.meta.env.VITE_PROJECT_BALLOTCONTRACT,
+  '0xActivityContract': import.meta.env.VITE_PROJECT_ACTIVITYCONTRACT,
+  '0xMemorials': import.meta.env.VITE_PROJECT_MEMORIALS
 })
 
 console.log(await config().all())
